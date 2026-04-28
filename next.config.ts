2cd4b965f55aa9e497d8abbe69436837d36f4537
Max Zimmermann <a12231371@unet.univie.ac.at>
2026-04-28 13:01:20 +0200

    changed next config for deploying on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/Pilates-Website" : "",
-  assetPrefix: isProd ? "/Pilates-Website/" : "",
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/Pilates-Website" : "",
   },
 };
 
